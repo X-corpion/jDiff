e8e0bf46a3cf2d17d8772d58bdd6cd888266a97d
Shawn Xu <xunnan.xu@gmail.com>
2017-11-19 22:48:30 -0800

    TRAVIS_BRANCH is the tag name if trigged by tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -exo pipefail
-if [[ ${TRAVIS_BRANCH} = 'master' ]] && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
+if ([[ ${TRAVIS_BRANCH} = 'master' ]] || [[ ${TRAVIS_BRANCH} = v* ]]) && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
     openssl aes-256-cbc -K ${encrypted_39dc14e92501_key} -iv ${encrypted_39dc14e92501_iv} -in signing.asc.enc -d | ${GPG_EXECUTABLE} --fast-import
-    if [[ ${TRAVIS_TAG} != rel/* ]]; then
+    if [[ ${TRAVIS_TAG} != v* ]]; then
         echo 'No Tag Defined. Deploying Snapshot.'
         mvn build-helper:parse-version versions:set -DnewVersion='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}-SNAPSHOT' versions:commit
     fi
