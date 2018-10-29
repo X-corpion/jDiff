3579742ed9bf5a040c51b183cdbb5bd4ee2dd01a
Shawn Xu <xunnan.xu@gmail.com>
2018-10-28 18:36:14 -0700

    add gitflow support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -exo pipefail
-if ([[ ${TRAVIS_BRANCH} = 'master' ]] || [[ ${TRAVIS_BRANCH} = v* ]]) && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
+if [[ ${TRAVIS_BRANCH} = release/* ]] && [[ ${TRAVIS_PULL_REQUEST} = 'false' ]]; then
     openssl aes-256-cbc -K ${encrypted_39dc14e92501_key} -iv ${encrypted_39dc14e92501_iv} -in signing.asc.enc -d | ${GPG_EXECUTABLE} --fast-import
-    if [[ ${TRAVIS_TAG} != v* ]]; then
-        echo 'No Tag Defined. Deploying Snapshot.'
-        mvn build-helper:parse-version versions:set -DnewVersion='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}-SNAPSHOT' versions:commit
-    fi
-    mvn -B -U -P ossrh -DskipTests=true --settings .travis-settings.xml deploy
+    mvn -B -U -P ossrh -DskipTests=true --settings .travis-settings.xml deploy gitflow:release-finish
 fi
